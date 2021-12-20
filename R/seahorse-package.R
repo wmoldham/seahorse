d08ef8bd6fd2ec00a9dbd53727f543f9301e2fa3
William Oldham <wm.oldham@gmail.com>
2021-12-20 10:55:44 -0500

    add package documentation

---
+++ R/seahorse-package.R
@@ -0,0 +1,9 @@
+#' @importFrom methods setClass setGeneric setMethod
+#' @keywords internal
+"_PACKAGE"
+
+# The following block is used by usethis to automatically manage
+# roxygen namespace tags. Modify with care!
+## usethis namespace: start
+## usethis namespace: end
+NULL
